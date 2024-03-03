e110118a074b6002fd57e68a5e43753aee703cda
Chokseys2022 <choksey2022@gmail.com>
2024-03-03 14:32:56 -0800

    route changed

--- utilities/data.js
+++ utilities/data.js
@@ -6,7 +6,7 @@ const essays = [
   { topic: "community service", group: "Latin Alliance", award: 500 },
   { topic: "sports", group: "CAAPA", award: 500 },
   { topic: "school clubs involvement", group: "KAAPA", award: 500 },
-  { topic: "success", group: "WHCC", award: 500 },
+  { topic: "success", group: "WHCC", award: 800 },
 ];
 
 module.exports = essays;
