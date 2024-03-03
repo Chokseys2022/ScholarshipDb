f550810c72844772ecf08e2b9a18fcfbae5fd897
Chokseys2022 <choksey2022@gmail.com>
2024-03-03 13:41:06 -0800

    recommender name changed

--- utilities/dataL.js
+++ utilities/dataL.js
@@ -1,12 +1,12 @@
 const letters = [
   { letter: true, recName: "Kathy Lee", gpa: 2.7 },
-  { letter: false, recName: "Jane Smith", gpa: 3.9 },
-  { letter: true, recName: "Ryan Z", gpa: 2.5 },
-  { letter: true, recName: "Tom Cruise", gpa: 2.8 },
-  { letter: true, recName: "John Doe", gpa: 3.5 },
+  { letter: false, recName: "Nathanial Miller", gpa: 3.9 },
+  { letter: true, recName: "Ryan Zhang", gpa: 2.5 },
+  { letter: true, recName: "Parneet Dhillon", gpa: 2.8 },
+  { letter: true, recName: "Sarina Shah", gpa: 3.5 },
   { letter: false, recName: "Jane Smith", gpa: 3.8 },
   { letter: true, recName: "Alison Johnson", gpa: 4.0 },
-  { letter: true, recName: "Jason Bourne", gpa: 2.8 },
+  { letter: true, recName: "Jason Chang", gpa: 2.8 },
 ];
 
 module.exports = letters;
--- utilities/dataS.js
+++ utilities/dataS.js
@@ -1,7 +1,7 @@
 const students = [
   {
     id: 1,
-    studentName: "Some Name",
+    studentName: "Kayla Lee",
     email: "email1@email.com",
     contact: 123 - 456 - 7890,
   },
@@ -13,37 +13,37 @@ const students = [
   },
   {
     id: 3,
-    studentName: "Yet Another Name",
+    studentName: "Derrick Chang",
     email: "email3@email.com",
     contact: 555 - 123 - 4567,
   },
   {
     id: 4,
-    studentName: "Fourth Student",
+    studentName: "Lily Zhao",
     email: "email4@email.com",
     contact: 111 - 222 - 3333,
   },
   {
     id: 5,
-    studentName: "Fifth Student",
+    studentName: "Jose Fucile",
     email: "email5@email.com",
     contact: 444 - 555 - 6666,
   },
   {
     id: 6,
-    studentName: "Sixth Student",
+    studentName: "Sophia Chang",
     email: "email6@email.com",
     contact: 777 - 888 - 9999,
   },
   {
     id: 7,
-    studentName: "Seventh Student",
+    studentName: "Calvin Li",
     email: "email7@email.com",
     contact: 101 - 202 - 3030,
   },
   {
     id: 8,
-    studentName: "Eighth Student",
+    studentName: "Tiffany Chen",
     email: "email8@email.com",
     contact: 404 - 505 - 6060,
   },
